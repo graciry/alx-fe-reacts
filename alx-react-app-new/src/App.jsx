62633a481e8050bf50835f3d12c8c32b63c05480
graciry <ngunyigwothaya@gmail.com>
2024-08-12 13:37:46 +0300

    Create simple counter application using useState in React

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 
@@ -42,17 +43,20 @@ function App() {
         <WelcomeMessage />
       </div>
       <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-        <div>
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <UserProfile name="Bob" age="30" bio="A passionate coder and gamer" />
-            <UserProfile name="Charlie" age="28" bio="Enjoys cooking and traveling" />
-        </div>
-        
-      
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+      <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <UserProfile name="Bob" age="30" bio="A passionate coder and gamer" />
+        <UserProfile name="Charlie" age="28" bio="Enjoys cooking and traveling" />
+      </div>
+      <div>
+        <Counter />
+      </div>
+
+
 
     </>
   )
