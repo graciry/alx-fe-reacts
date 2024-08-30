7849150074a365f2d6a531b7a049a24ea9305ef6
graciry <ngunyigwothaya@gmail.com>
2024-08-30 12:46:42 +0300

    Added react query demo

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,30 @@
+// src/components/PostsComponent.js
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
