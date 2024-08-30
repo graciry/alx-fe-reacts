373ade55333a44dcf45a1ec5b36419e2f2767bf3
graciry <ngunyigwothaya@gmail.com>
2024-08-30 17:18:24 +0300

    ADD

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> 
         <Route
           path="/profile/*"
           element={
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function BlogPost() {
-  const { postId } = useParams();
-  return <div>Blog Post ID: {postId}</div>;
+  const { id } = useParams(); // Extract the dynamic parameter from the URL
+
+  return (
+    <div>
+      <h2>Blog Post</h2>
+      <p>Blog Post ID: {id}</p>
+    </div>
+  );
 }
 
 export default BlogPost;
